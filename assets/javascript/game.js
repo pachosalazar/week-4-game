6f7e35c3e363129200aedff81277cc127b5f9ffe
Pachosalazar <jfsh87@hotmail.com>
2018-03-09 12:38:37 -0500

    starting js and html

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+// There will be four crystals displayed as buttons on the page.
+
+// The player will be shown a random number at the start of the game.
+
+// When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
+
+// Your game will hide this amount until the player clicks a crystal.
+// When they do click one, update the player's score counter.
+// The player wins if their total score matches the random number from the beginning of the game.
+
+// The player loses if their score goes above the random number.
+
+// The game restarts whenever the player wins or loses.
+
+// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+// Option 1 Game design notes
+// The random number shown at the start of the game should be between 19 - 120.
+
+// Each crystal should have a random hidden value between 1 - 12.
+
+// I need to create the var that i will use 
+var rnumber =[];
+var win=[];
+var losses=[];
+var score=0;
+var cristals=[];
+
+// try to create crystals by jquery ans js
+for (var i = 0; i < 4; i++){
+
+    var crystal =$("<div>");
+
+    $(".crystals").append(crystal);
+
+}
+// i have to create a randon number for the pc choice 
+var pcn = rnumber[Math.floor((Math.random() * 101 ) + 19 )];
+
